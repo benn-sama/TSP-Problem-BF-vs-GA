da00436c5c1057d9cca164b7c3342cb48685b9fd
benn-sama <green.l.benjamin04@gmail.com>
2024-10-25 17:56:01 -0500

    cleaned repository

--- adjacency.cpp
+++ adjacency.cpp
@@ -1,27 +1,31 @@
 #include "adjacency.hpp"
 #include <stdexcept>
 
+// defaule constructor
 Adjacency::Adjacency() {
       matrix = new double*[ROWS];
+
       for (int i = 0; i < ROWS; ++i) {
           matrix[i] = new double[COLUMNS];
-          // Initialize all elements to infinity or a sentinel value
+          // Initialize all elements to 0.0
           for (int j = 0; j < COLUMNS; ++j) {
               matrix[i][j] = 0.0;
           }
       }
 }
 
+// initializes matrix with data from a file assuming each line ONLY contains a double
 void Adjacency::initializeArray(std::ifstream* fileStream) {
     std::string line;
     
-    // Read upper triangular part of the matrix
+    // Read upper triangular part of the matrix 
     for (int i = 0; i < ROWS - 1; ++i) {
         for (int j = i + 1; j < COLUMNS; ++j) {
             if (!std::getline(*fileStream, line)) {
                 throw std::runtime_error("Incomplete matrix data in file");
             }
             
+            // tokenizes values from the file and assigns it into a double
             std::istringstream iss(line);
             double value;
             if (!(iss >> value)) {
@@ -35,13 +39,16 @@ void Adjacency::initializeArray(std::ifstream* fileStream) {
     }
 }
 
+// returns travel time (edge of adjacent vertexes)
 double Adjacency::returnTravelTime(int fromCity, int toCity) {
+    // also checks out of bounds
     if (fromCity < 0 || fromCity >= ROWS || toCity < 0 || toCity >= COLUMNS) {
     }
     
     return matrix[fromCity][toCity];
 }
 
+// prints all weights for each edge to verify if it is correct
 void Adjacency::printAll() {
   for (int i = 0; i < this->ROWS; ++i) {
     for (int j = 0; j < this->COLUMNS; ++j) {
@@ -55,5 +62,6 @@ Adjacency::~Adjacency() {
   for (int i = 0; i < ROWS; ++i) {
     delete[] matrix[i];
   }
+  
   delete[] matrix;
 }
